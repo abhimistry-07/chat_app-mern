ac26cfb33dbfec433435ee503114611ffcf3f70d
Abhishek <mistryabhishek2000@gmail.com>
2024-02-18 19:36:12 +0530

    fetch chats part added

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -50,8 +50,25 @@ chatRoute.post('/', async (req, res) => {
     }
 });
 
-chatRoute.get('/', (req, res) => {
-
+chatRoute.get('/', async (req, res) => {
+    try {
+        chatModel.find({
+            users: { $elemMatch: { $eq: req.user._id } }
+        })
+            .populate('users', '-password')
+            .populate('groupAdmin', '-password')
+            .populate('latestMessage')
+            .sort({ updatedAt: -1 })
+            .then(async (results) => {
+                results = await userModel.populate(results, {
+                    path: 'latestMessage.sender',
+                    select: 'name pic email',
+                });
+                res.status(200).send(results);
+            });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 chatRoute.post('/group', (req, res) => {

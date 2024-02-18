f83cf22b0ce2d171ac7e3d7672ddf0ce50055db2
Abhishek <mistryabhishek2000@gmail.com>
2024-02-19 00:19:10 +0530

    rename group part added

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -103,8 +103,22 @@ chatRoute.post('/group', async (req, res) => {
     }
 });
 
-chatRoute.put('/rename', (req, res) => {
+chatRoute.put('/rename', async (req, res) => {
+    const { chatId, chatName } = req.body;
 
+    const updatedChat = await chatModel.findByIdAndUpdate(chatId,
+        { chatName },
+        { new: true }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password")
+
+
+    if (!updatedChat) {
+        res.status(400).send('Chat not found!');
+    } else {
+        res.status(200).json(updatedChat);
+    }
 });
 
 chatRoute.put('/removefrgrp', (req, res) => {
